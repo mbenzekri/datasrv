7904dbf6ab547d948a2e62e70946bbddbc3f1e32
Mohamed <mbenzekri@users.noreply.github.com>
2019-12-04 15:54:12 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,59 +4,41 @@ const express = require('express')
 const compression = require('compression')
 const port = process.env.PORT || 1337;
 
-console.log("Server running at http://localhost:%d", port);
-
-
-console.log(`${(new Date()).toISOString()} : SAMIDATA PORT IS  ${port}`);
+console.log(`==========> ${(new Date()).toISOString()} : SAMIDATA STARTING ON PORT ${port}`);
 
 const app = express()
 
-var logger = function (req, res, next) {
+function logger (req, res, next) {
     console.log(`${(new Date()).toISOString()} : SAMIDATA REQUEST ${req.url}`);
     next()
-};
+}
 
 var check = function (req, res, next) {
     if (!config.useauth) return next()
     if (config.validateKey(req.headers['X-sami-token'], req.url)) {
-        console.log(`${(new Date()).toISOString()}: X-sami-token ${req.headers['X-sami-token']} for ${req.url}`);
+        console.log(`==========> ${(new Date()).toISOString()}: SAMIDATA Authorized request  ${req.headers['X-sami-token']} for ${req.url} `);
         next();
     } else {
-        console.log(`${(new Date()).toISOString()}: Unauthorized request ${req.url} `);
+        console.log(`==========> ${(new Date()).toISOString()}: SAMIDATA Unauthorized request ${req.url} `);
         res.sendStatus(401)
     }
-};
+}
 
-app.use(compression({
-    threshold: 1024, // sous cette limite les fichiers ne sont pas compressé
-    filter: function (req, res) {
-        var ct = res.get('content-type');
-        // return `true` for content types that you want to compress,
-        // `false` otherwise
-        return true;
-    }
-}))
+app.use(compression())
 
 app.use(logger)
 let state = 'no error thrown !'
-app.use('/data/geo', check)
-// try {
+app.use('/geo', check)
 let request = require('request');
-app.use('/data/geo', function (req, res) {
+app.use('/geo', function (req, res) {
     const bloburl = `${config.geocont}${req.path}${config.sastoken}`
 
-    console.log(`${(new Date()).toISOString()}: Proxying ${req.url} to ${bloburl}`);
+    console.log(`==========> ${(new Date()).toISOString()}: SAMIDATA Proxying ${req.url} to ${bloburl}`);
     res.status(200)
     req.pipe(request(bloburl)).pipe(res);
-    })
-// } catch (e) {
-//     state = `error  thrown !!! <br> ${e.toString()}`
-// }
-// app.use('/', (request, response) => {
-//     response.writeHead(200, { "Content-Type": "text/plain" });
-//     response.end(`Hello World! (node ${process.version})  \n ${state} `);
-// })
+})
+
 
 
 http.createServer(app).listen(port)
-console.log(`${(new Date()).toISOString()}: SAMIDATA SERVER running!`);
+console.log(`==========>  ${(new Date()).toISOString()}: SAMIDATA SERVER running!`);
